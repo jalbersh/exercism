c0f52c55f23d31e352f2c1cb88ee70ea2a4f3ad4
praneeth.tangallapal <praneeth.tangallapal@dish.com>
2016-12-08 13:04:02 -0700

    implemented leap

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -2,30 +2,36 @@
 // except every year that is evenly divisible by 100
 // unless the year is also evenly divisible by 400
 
-export class Year {
+var Year = function(year) {
+    this.year = year;
+    console.log('Year instantiated');
+}
 
-    constructor(year) {
-        this.year = year
-    }
+var Year = function(year1, year2, a, b, c){
+    this.year = year1
+    this.year2 = year2
+    console.log('year-2arg instantiated')
+}
 
-    isLeap() {
-        console.log('year=', this.year)
-        if (this.year % 4) {
-            console.log('divided by 4')
-            if (this.year % 100) {
-                console.log('divided by 100')
-                if (this.year % 400) {
-                    console.log('divided by 400')
-                    return true
-                }
-                console.log('not divided by 400, but is divided by 1--')
-                return false
+Year.prototype.isLeap = function() {
+    console.log('year=', this.year)
+    if (this.year % 4 == 0) {
+        console.log('divided by 4')
+        if (this.year % 100 == 0) {
+            console.log('divided by 100')
+            if (this.year % 400 == 0) {
+                console.log('divided by 400')
+                return true
             }
-            console.log('not divided by 100')
-            return true
-        } else {
-            console.log('not divided by 4')
+            console.log('not divided by 400, but is divided by 1--')
             return false
         }
+        console.log('not divided by 100')
+        return true
+    } else {
+        console.log('not divided by 4')
+        return false
     }
 }
+
+module.exports = Year;
