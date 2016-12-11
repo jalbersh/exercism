bf9bb254efa90389bc5e2802327b8efd9c6d1379
James Albersheim <james@albersheim.com>
2016-12-11 12:19:19 -0700

    finalized solution for word-count

---
+++ javascript/word-count/word-count.js
@@ -0,0 +1,25 @@
+var Words = function () {}
+
+Words.prototype.count = function(input) {
+    input = input.toLowerCase()
+    input = input.replace(/[?¿.\/#¡!$%\^&\*;:{}=\-_`~()@]/g,"")
+    input = input.replace(/\s+/g, ' ')
+    input = input.replace(/,/g,' ')
+    if (input[0] == ' ') {
+        input = input.substring(1)
+    }
+    if (input[input.length-1] == ' ') {
+        input = input.substring(0,input.length-1)
+    }
+    subStrings = input.split(' ');
+    var counts = Object.create(null);
+    subStrings.forEach(function(string) {
+        if (typeof counts[string] === 'undefined') {
+            counts[string] = 0;
+        }
+        counts[string]++;
+    });
+    return counts;
+};
+
+module.exports = Words;
