db784dfd506c7bbd522009e1cdb349c224c3d08a
James Albersheim <james@albersheim.com>
2017-02-16 19:35:00 -0700

    finalized solution to ocr-numbers

--- javascript/ocr-numbers/ocr-numbers.js
+++ javascript/ocr-numbers/ocr-numbers.js
@@ -44,23 +44,18 @@ var DIGITS = [
     ]
 
 function findDigit(input) {
-    console.log('in findDigit with [',input,']')
     for (var ctr=0;ctr<10;ctr++) {
         var digit = DIGITS[ctr]
-        //console.log('input.length=',input.length+';digit.length=',digit.length)
         if (digit == input) {
-            console.log('found digit=',ctr)
             return ctr.toString()
         }
     }
     // remove extra newline
     var len = input.length
     input = input.substring(0,len-1)
-    //console.log('input.length=',input.length,'input=[',input,']')
     for (var ctr=0;ctr<10;ctr++) {
         var digit = DIGITS[ctr]
         if (digit == input) {
-            console.log('found digit=',ctr)
             return ctr.toString()
         }
     }
@@ -70,35 +65,31 @@ function findDigit(input) {
 function convert(input) {
     var digit = findDigit(input)
     if (digit != '-1') return digit
-    console.log('input',input)
     var lines = input.split('\n')
     var digits = ''
-    var sdigit1 = ''
-    var sdigit2 = ''
+    var sdigit = ['','','','','','','','','','']
     lines.forEach(function(line) {
+        var found = false
         var len = line.length
         var width = Math.ceil(len/WIDTH)
-        console.log('line len=',len,';line=',line)
         var ctr=0
-        //for (;ctr<width;ctr++) {
+        for (;ctr<width;ctr++) {
             var part1 = line.substring(ctr * WIDTH, WIDTH * (ctr + 1))
-            //var part1 = line.substring(0,3)
-            console.log('part1=',part1)
-            sdigit1 += part1 + '\n'
-            var spart1 = findDigit(sdigit1)
+            sdigit[ctr] += part1 + '\n'
+            var spart1 = findDigit(sdigit[ctr])
             if (spart1 != '-1') {
+                found = true
                 digits += spart1
-            } 
-        //}
-        ctr++
-        var part2 = line.substring(ctr * WIDTH, WIDTH * (ctr + 1))
-        //var part2 = line.substring(3,6)
-        sdigit2 += part2+'\n'
-        var spart2 = findDigit(sdigit2)
-        if (spart2 != '-1') {
-            digits += spart2
+            } else {
+                digits += '?'
+            }
         }
     })
+    var i=0
+    while (digits[i] == '?') {
+        i++
+    }
+    digits = digits.substring(i)
     if (digits == '') return '?'
     return digits
 }
