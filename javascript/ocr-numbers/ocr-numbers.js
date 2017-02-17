5f05b215789e671f97dd0e1551d644b76e0e6821
james albersheim <james@albersheim.com>
2017-02-16 17:19:21 -0700

    try#3

--- javascript/ocr-numbers/ocr-numbers.js
+++ javascript/ocr-numbers/ocr-numbers.js
@@ -1,4 +1,4 @@
-var WIDTH=4
+var WIDTH=3
 var HEIGHT=3
 var DIGITS = [
         ' _ \n' +
@@ -47,18 +47,20 @@ function findDigit(input) {
     console.log('in findDigit with [',input,']')
     for (var ctr=0;ctr<10;ctr++) {
         var digit = DIGITS[ctr]
-        console.log('input.length=',input.length+';digit.length=',digit.length)
+        //console.log('input.length=',input.length+';digit.length=',digit.length)
         if (digit == input) {
+            console.log('found digit=',ctr)
             return ctr.toString()
         }
     }
     // remove extra newline
     var len = input.length
     input = input.substring(0,len-1)
-    console.log('input.length=',input.length,'input=[',input,']')
+    //console.log('input.length=',input.length,'input=[',input,']')
     for (var ctr=0;ctr<10;ctr++) {
         var digit = DIGITS[ctr]
         if (digit == input) {
+            console.log('found digit=',ctr)
             return ctr.toString()
         }
     }
@@ -73,16 +75,24 @@ function convert(input) {
     var digits = ''
     var sdigit1 = ''
     var sdigit2 = ''
-    var start = 0
-    var end = WIDTH
     lines.forEach(function(line) {
-        var part1 = line.substring(0,3)
-        sdigit1 += part1+'\n'
-        var spart1 = findDigit(sdigit1)
-        if (spart1 != '-1') {
-            digits += spart1
-        }
-        var part2 = line.substring(3,6)
+        var len = line.length
+        var width = Math.ceil(len/WIDTH)
+        console.log('line len=',len,';line=',line)
+        var ctr=0
+        //for (;ctr<width;ctr++) {
+            var part1 = line.substring(ctr * WIDTH, WIDTH * (ctr + 1))
+            //var part1 = line.substring(0,3)
+            console.log('part1=',part1)
+            sdigit1 += part1 + '\n'
+            var spart1 = findDigit(sdigit1)
+            if (spart1 != '-1') {
+                digits += spart1
+            } 
+        //}
+        ctr++
+        var part2 = line.substring(ctr * WIDTH, WIDTH * (ctr + 1))
+        //var part2 = line.substring(3,6)
         sdigit2 += part2+'\n'
         var spart2 = findDigit(sdigit2)
         if (spart2 != '-1') {
